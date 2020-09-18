3e11559bb5815988a644b767af30c25779763164
Dylan Olin <olindyla@gmail.com>
2020-09-18 15:12:26 -0600

    viewerjs

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -7,6 +7,10 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import Tiff from "tiff.js";
+import 'viewerjs/dist/viewer.css';
+import ViewerJS from 'viewerjs';
+import dog from './dog.jpg'
+import otherDog from './other-dog.jpg'
 
 // let ftpUrl =
 //   "http://ftp.gallatin.mt.gov/Commission%20Minutes/Commissioner%20Journals/";
@@ -39,12 +43,37 @@ async function getTiffCanvas(href: string):Promise<HTMLCanvasElement> {
   });
 }
 
+async function getTiffImage(href: string):Promise<string> {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", href);
+    xhr.responseType = "arraybuffer";
+    xhr.onload = function (e) {
+      var buffer = xhr.response;
+      var tiff = new Tiff({ buffer: buffer });
+      let aB = tiff.readRGBAImage();
+      console.log(otherDog)
+      var blob = new Blob( [ aB ], { type: "image/jpeg" } );
+      var urlCreator = window.URL || window.webkitURL;
+      var imageUrl = urlCreator.createObjectURL( blob );
+
+      resolve(imageUrl);
+    };
+    xhr.send();
+  });
+}
+
 class Viewer extends React.Component<
   { classes; selected?: { href?: string }; ref?: any },
   ViewerState
 > {
+  viewer:ViewerJS | undefined;
+  viewerRef: React.RefObject<HTMLImageElement>;
+  imageUrl:string;
   constructor(props) {
     super(props);
+    this.viewerRef = React.createRef();
+    this.imageUrl = "./dog.jpg"
   }
 
   async update(content) {
@@ -54,13 +83,27 @@ class Viewer extends React.Component<
     (this.refs.canvasRef as HTMLCanvasElement).width = canvas.width;
     (this.refs.canvasRef as HTMLCanvasElement).height = canvas.height;
     let ctx =(this.refs.canvasRef as HTMLCanvasElement).getContext('2d')
-    ctx?.drawImage( canvas,0,0)
-
-    //this.setState({ canvas, loading: false });
+    //ctx?.drawImage( canvas,0,0);
+    let image = await getTiffImage(href);
+    if (this.viewer){
+      this.viewer.destroy();
+      if (this.viewerRef.current){
+        this.viewerRef.current.src = image;
+      }
+      
+      this.viewer= new ViewerJS((this.viewerRef.current as Element),{
+        inline:true
+      })
+    }
+    this.setState({loading: false });
   }
 
   async componentDidMount() {
     const image = "an image";
+    this.viewer= new ViewerJS((this.viewerRef.current as Element),{
+      inline:true
+    })    
+  
     this.setState({ loading: false });
   }
 
@@ -69,7 +112,7 @@ class Viewer extends React.Component<
 
     return (
       <div>
-        <CircularProgress hidden={!this.state?.loading}></CircularProgress>
+        <div ><img ref={this.viewerRef} src={dog}></img></div>
         <canvas ref="canvasRef"/>
       </div>
     );
